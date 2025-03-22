d33abddb1ca36a04934831f5e55ea4e1156b3cc7
Rawann0m <raawannm@gmail.com>
2025-03-22 14:20:39 +0300

    Initial Commit

---
+++ NewsAppWeeklyTask/NewsAppWeeklyTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppWeeklyTaskApp.swift
+//  NewsAppWeeklyTask
+//
+//  Created by Rawan on 22/09/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppWeeklyTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
